1b46985345bddc17f46f8e576a30e24b26e9a9df
smolskig <contatosmolski@gmail.com>
2021-09-14 00:18:12 -0300

    removing populate

--- src/contollers/spellController.js
+++ src/contollers/spellController.js
@@ -2,42 +2,13 @@ const Spell = require('../models/spell');
 const axios = require('axios')
 
 module.exports = {
-    async populate(req,res){
-        axios.get('https://dnd5spells.rpgist.net/api/v1.1/pt-BR/rules')
-            .then(async SpellResponse=>{    
-                Spell.collection.drop()
-                let result = []
-                SpellResponse.data.spells.map((info=>{
-                    const regex = /(<([^>]+)>)/ig
-                    let alcance = info.range / 3.2808
-                    const formated = {
-                        "nome":info.name,
-                        "tempo_conjuracao":`${info.casting_time} ${info.casting_time_unit}`,
-                        "alcance":`${alcance.toFixed(1)} Metros`,
-                        "componentes":info.material_component,
-                        "duracao":`${info.duration} ${info.duration_unit} `,
-                        "descricao":info.description.replace(regex, ""),
-                        "niveis_superiores":"teste"
-                    }
-                    result.push(formated)    
-                }))
-                try{
-                    await result.map(async response=>{
-                        Spell.create(response)
-                            .then(()=>{
-                            })
-                            .catch((err)=>console.log(err))
-                    })
-                    return res.sendStatus(200)
-                }
-                catch{
-                    return res.error('ocorreu um erro ao popular o banco')
-                }  
-            })   
-    },
-
     async getSpells(req,res){
         const data = await Spell.find()
+        return res.json(data)
+    },
+
+    async getBySpellName(req, res){
+        const data = await Spell.find({"nome": req.params.spellName})        
         return res.json(data)
     }
 }
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,6 @@ const SpellController = require('./contollers/spellController')
 const routes = express.Router();
 
 routes.get('/api/spells',SpellController.getSpells);
-routes.get('/api/populate',SpellController.populate);
+routes.get('/api/spells/:spellName',SpellController.getBySpellName);
 
 module.exports = routes
